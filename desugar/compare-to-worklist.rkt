2ac10843db5b57f6899403460ffaea07af50e936
Thomas Gilray <thomas.gilray@gmail.com>
2014-04-09 20:42:05 -0700

    comparison again

--- desugar/compare-to-worklist.rkt
+++ desugar/compare-to-worklist.rkt
@@ -266,8 +266,8 @@
 
 (define sigma (explore (set root) (hash)))
 
-(pretty-print sigma)
-(exit)
+;(pretty-print sigma)
+;(exit)
 
 
 ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
